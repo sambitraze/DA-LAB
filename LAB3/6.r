02fb6bc6034c579f6dd53449e7a75c9f562aa4d6
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-02-04 22:43:54 +0530

    Update 6.r

--- LAB3/6.r
+++ LAB3/6.r
@@ -1,22 +1,21 @@
-a <- c()
+marks <- c()
 for (x in 1:3) {
-    print(paste("For subject ", x))
-    n <- as.integer(readline(prompt = "Enter a marks:"))
-    a <- c(a, n)
+  print(paste("for subject ", x))
+  n <- as.integer(readline(prompt = "enter marks: "))
+  marks <- c(marks, n)
 }
-print(paste("Total is :", sum(a)))
-print(paste("Average is :", mean(a)))
-
-if (mean(a) >= 90) {
-    print("O")
-} else if (mean(a) >= 80) {
-    print("A")
-} else if (mean(a) >= 70) {
-    print("B")
-} else if (mean(a) >= 60) {
-    print("C")
-} else if (mean(a) >= 50) {
-    print("D")
+print(paste("total marks are: ", sum(marks)))
+print(paste("average marks are: ", mean(marks)))
+if (mean(marks) >= 90) {
+  print("O")
+} else if (mean(marks) >= 80) {
+  print("A")
+} else if (mean(marks) >= 70) {
+  print("B")
+} else if (mean(marks) >= 60) {
+  print("C")
+} else if (mean(marks) >= 50) {
+  print("D")
 } else {
-    print("F")
+  print("F")
 }
